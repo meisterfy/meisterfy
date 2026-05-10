7829c4210baf22900a17a5dc9a3629ec4dcae4d2
Rafhael Marsigli <rmarsigli@gmail.com>
2026-05-10 03:04:41 -0300

    fix: add LogoPNG to IntegrationSchema and update googleads/meta schemas

--- backend/internal/connector/meta/schema.go
+++ backend/internal/connector/meta/schema.go
@@ -2,16 +2,20 @@ package meta
 
 import (
 	"context"
+	_ "embed"
 
 	"github.com/rush-maestro/rush-maestro/internal/connector"
 	"github.com/rush-maestro/rush-maestro/internal/domain"
 )
 
+//go:embed logo.svg
+var logoSVG string
+
 func init() {
 	connector.RegisterProvider(&connector.IntegrationSchema{
 		Provider:    domain.ProviderMeta,
 		Group:       domain.GroupSocialMedia,
-		DisplayName: "Meta (Instagram / Facebook)",
+		DisplayName: "Meta",
 		Description: "Publish posts and manage ads on Instagram and Facebook.",
 		LogoSVG:     logoSVG,
 		CredentialFields: []connector.FieldSchema{
@@ -55,5 +59,3 @@ func init() {
 		},
 	})
 }
-
-const logoSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="#1877F2" d="M48 24C48 10.7 37.3 0 24 0S0 10.7 0 24c0 12 8.8 21.9 20.3 23.7V30.9h-6.1V24h6.1v-5.3c0-6 3.6-9.3 9-9.3 2.6 0 5.3.5 5.3.5v5.9h-3c-2.9 0-3.8 1.8-3.8 3.7V24h6.5l-1 6.9h-5.4v16.8C39.2 45.9 48 36 48 24z"/></svg>`
