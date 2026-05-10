94b550fd8007f03454bf3eeaafb1d20f2027a11d
Rafhael Marsigli <rmarsigli@gmail.com>
2026-05-10 03:05:13 -0300

    refactor: simplify settings/general loader to delegate to parent

--- frontend/src/routes/[tenant]/settings/general/+page.ts
+++ frontend/src/routes/[tenant]/settings/general/+page.ts
@@ -1,22 +1,6 @@
-import { getTenant } from '$lib/api/tenants'
-import { error } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
-
-export const load: PageLoad = async ({ params, fetch }) => {
-	const tenant = await getTenant(params.tenant, fetch).catch(() => null)
-	if (!tenant) error(404, 'Client not found')
-
-	return {
-		tenant: params.tenant,
-		client: {
-			id: tenant.id,
-			brand: { name: tenant.name, niche: tenant.niche, google_ads_id: tenant.google_ads_id }
-		},
-		brand: {
-			name: tenant.name,
-			niche: tenant.niche ?? '',
-			google_ads_id: tenant.google_ads_id ?? ''
-		}
-	}
+export const load: PageLoad = async ({ parent }) => {
+	const { tenant, client } = await parent()
+	return { tenant, brand: client.brand }
 }
