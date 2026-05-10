f37e12444e9d9beeb63a8f513f6b7837d6ffff06
Rafhael Marsigli <rmarsigli@gmail.com>
2026-05-10 03:05:42 -0300

    refactor: restyle select components to match project design system

---
+++ frontend/src/lib/components/ui/provider-icon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let {
+		provider,
+		logoSvg = '',
+		logoPng = '',
+		class: className = ''
+	} = $props<{
+		provider: string
+		logoSvg?: string
+		logoPng?: string
+		class?: string
+	}>()
+</script>
+
+{#if logoSvg}
+	<div class="object-contain [&>svg]:h-full [&>svg]:w-full [&>svg]:object-contain {className}">
+		{@html logoSvg}
+	</div>
+{:else if logoPng}
+	<img 
+		src={logoPng.startsWith('data:') ? logoPng : `data:image/png;base64,${logoPng}`} 
+		alt={provider} 
+		class="object-contain {className}"
+	/>
+{/if}
+
